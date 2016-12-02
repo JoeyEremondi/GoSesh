8ab0bdaee94b3c67f59da9964e9e969f66c01df6
jspacek <jodi.spacek@gmail.com>
2016-12-02 11:11:12 -0800

    Rework generation test and code for Send Receive. Cleanup of commented out code. TODO: parse globaltype and return a skeleton

--- generation/generation.go
+++ generation/generation.go
@@ -1,34 +1,63 @@
 package generation
 
-import "bytes"
+import (
+	"reflect"
 
-// Channel is a struct for the developer to use
+	"github.com/JoeyEremondi/GoSesh/multiparty"
+)
+
+// Channel type here is a wrapper for the developer to use
 type Channel struct {
 	Name        string
 	Source      string
 	Destination string
 }
 
-// ValueType is a struct for the developer to use
-type ValueType struct {
-	Type string // instead of Sort which is non-intuitive
+// MessageType is a wrapper for the developer to use
+type MessageType struct {
+	// Call it a type instead of "value of a certain Sort"
+	// which is non-intuitive to the developer
+	Type string
 }
 
-// LocalType is a struct for the developer to use
-type LocalType struct {
-	// TODO What forms can this take on? Does this need some interface?
-	Anything string
+// Event is a wrapper for the developer to use
+type Event struct {
+	// Wrap the global type into an event for easier comprehension
+	Event string //ValueType
 }
 
-//LocalSendType : perform unwrapping for the developer
-func LocalSendType(channel Channel, valueType ValueType, next LocalType) string {
-	return channel.Source + " --> " + channel.Destination + " : " + channel.Name
+//Send : wrap this into a ValueType with a Participant for source & dest
+func Send(channel Channel, messageType MessageType, nextEvent Event) string {
+
+	participant1 := multiparty.Participant(channel.Source)
+	participant2 := multiparty.Participant(channel.Destination)
+	multipartyChannel := multiparty.Channel(channel.Name)
+
+	prefix := multiparty.Prefix{
+		P1:       participant1,
+		P2:       participant2,
+		PChannel: multipartyChannel}
+
+	sort := multiparty.Sort(messageType.Type)
+
+	// TODO how to parse a generic event from user into global type
+	//next := nextEvent.Event
+
+	send := multiparty.ValueType{
+		ValuePrefix: prefix,
+		Value:       sort,
+		ValueNext:   nil}
+
+	// TODO how to output the stub?
+	return "/n" + reflect.ValueOf(send.Value).String() + " --> "
 }
 
-//LocalReceiveType : perform unwrapping for the developer
-func LocalReceiveType(channel Channel, valueType ValueType, next LocalType) string {
+//Receive : wrap this into a ValueType with a Participant for dest & source
+func Receive(channel Channel, messageType MessageType, nextEvent Event) string {
 	return channel.Destination + " --> " + channel.Source + " : " + channel.Name
 }
+
+/*
 
 //LocalBranchingType : perform unwrapping for the developer
 func LocalBranchingType(channel Channel, branches map[string]LocalType) string {
@@ -59,3 +88,4 @@ func LocalSelectionType(channel Channel, branches map[string]LocalType) string {
 
 // TODO are we implementing the LocalRecursiveType type?
 // TODO implicitly end the operations for the dev LocalEndType
+*/
--- generation/generation_test.go
+++ generation/generation_test.go
@@ -3,7 +3,7 @@ package generation
 import (
 	"testing"
 
-	"github.com/joey/sessions/generation"
+	"github.com/JoeyEremondi/GoSesh/generation"
 )
 
 // Run a single test file with verbose comments like:
@@ -13,10 +13,11 @@ func TestLocalSendType(test *testing.T) {
 		Name:        "testchannel",
 		Source:      "A",
 		Destination: "B"}
-	valueType := generation.ValueType{Type: "int"}
-	localType := generation.LocalType{Anything: "nexttype"}
 
-	localSendType := generation.LocalSendType(channel, valueType, localType)
+	valueType := generation.MessageType{Type: "int"}
+	Event := generation.Event{Event: "nexttype"}
+
+	localSendType := generation.Send(channel, valueType, Event)
 	test.Log(localSendType)
 }
 
@@ -25,26 +26,27 @@ func TestLocalReceiveType(test *testing.T) {
 		Name:        "testchannel",
 		Source:      "A",
 		Destination: "B"}
-	valueType := generation.ValueType{Type: "int"}
-	localType := generation.LocalType{Anything: "nexttype"}
+	valueType := generation.MessageType{Type: "int"}
+	Event := generation.Event{Event: "nexttype"}
 
-	localReceiveType := generation.LocalReceiveType(channel, valueType, localType)
+	localReceiveType := generation.Receive(channel, valueType, Event)
 	test.Log(localReceiveType)
 }
 
+/*
 func TestLocalBranchingType(test *testing.T) {
 	channel := generation.Channel{Name: "testchannel"}
-	localType1 := generation.LocalType{Anything: "localtype1"}
-	localType2 := generation.LocalType{Anything: "localtype2"}
-	localType3 := generation.LocalType{Anything: "localtype3"}
+	event1 := generation.Event{Event: "Event1"}
+	event2 := generation.Event{Event: "Event2"}
+	event3 := generation.Event{Event: "Event3"}
 	// TODO what is this key supposed to be used for?
 
-	localTypeMap := map[string]generation.LocalType{
-		"1": localType1,
-		"2": localType2,
-		"3": localType3}
-	localBranchingType := generation.LocalBranchingType(channel, localTypeMap)
+	EventMap := map[string]generation.Event{
+		"1": event1,
+		"2": event2,
+		"3": event3}
+	localBranchingType := generation.Branch(channel, EventMap)
 	test.Log(localBranchingType)
-}
+}*/
 
 // TODO test selection type
