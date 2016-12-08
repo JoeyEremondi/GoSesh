ce1f71a0c448768c272afab35e820651af9f4258
Joey Eremondi <joey.eremondi@gmail.com>
2016-12-08 15:47:17 -0800

    Add new test in its own folder

---
+++ example/loopUntilGood/loopUntilGood.go
@@ -0,0 +1,35 @@
+package main
+
+import "github.com/JoeyEremondi/GoSesh/mockup"
+
+// ASendToBThenBToC : A sends an int message to B, B sends the message to C
+func MakeLoopStub() {
+
+	channelAToB := mockup.Channel{
+		Name:        "fromAtoB",
+		Source:      "A",
+		Destination: "B"}
+
+	channelBToA := mockup.Channel{
+		Name:        "fromBtoA",
+		Source:      "B",
+		Destination: "A"}
+
+	mockup.CreateStubProgram("ABCExample",
+		mockup.Loop("testLoop",
+			mockup.Send(channelAToB, mockup.MessageType{Type: "int"}),
+			mockup.Switch(channelBToA,
+				mockup.Case("intIsGood",
+					mockup.Break(),
+				),
+				mockup.Case("intIsBad",
+					mockup.Continue("testLoop"),
+				),
+			),
+		),
+	)
+}
+
+func main() {
+	MakeLoopStub()
+}
