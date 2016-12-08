f0f7fb670b54906f4adb94df8494e9f13458ea09
Joey Eremondi <joey.eremondi@gmail.com>
2016-12-08 14:05:26 -0800

    Got code working to alter main function

--- mockup/makeStubs.go
+++ mockup/makeStubs.go
@@ -1,19 +1,48 @@
 package mockup
 
-import "go/ast"
+import (
+	"bytes"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+)
 
-func removeMain(n ast.Node) {
+func modifyMain(n ast.Node) {
 	ast.Inspect(n, func(argNode ast.Node) bool {
 		switch innerN := argNode.(type) {
+		//Rename our main, so we can add a different main
 		case *ast.FuncDecl:
 			if innerN.Name.String() == "main" {
-				innerN.Name.Name = "oldm"
-				return false
-			} else {
+				innerN.Name.Name = "makeGlobal"
+			}
+
+		case *ast.SelectorExpr:
+			switch pkgIdent := innerN.X.(type) {
+			case *ast.Ident:
+				if pkgIdent.Name == "mockup" && innerN.Sel.Name == "CreateStubProgram" {
+					innerN.Sel.Name = "setGlobalType"
+					pkgIdent.Name = "main"
+				}
+			default:
 				return true
 			}
+
 		default:
 			return true
 		}
+		return true
 	})
+}
+
+func main() {
+	fset := token.NewFileSet() // positions are relative to fset
+	f, err := parser.ParseFile(fset, "../example/loopUntilGood/loopUntilGood.go", nil, 0)
+	if err != nil {
+		panic(err)
+	}
+	modifyMain(f)
+	var buf bytes.Buffer
+	printer.Fprint(&buf, fset, f)
+	println(buf.String())
 }
