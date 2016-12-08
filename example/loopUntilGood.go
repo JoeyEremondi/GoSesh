0f6bb42998ec45a4d9ba90acd85e406f09db4cc3
Joey Eremondi <joey.eremondi@gmail.com>
2016-12-07 18:29:24 -0800

    Add example from multiparty_test.go as mockup

--- example/loopUntilGood.go
+++ example/loopUntilGood.go
@@ -3,7 +3,7 @@ package main
 import "github.com/JoeyEremondi/GoSesh/mockup"
 
 // ASendToBThenBToC : A sends an int message to B, B sends the message to C
-func ASendToBThenBToC() {
+func MakeLoopStub() {
 
 	channelAToB := mockup.Channel{
 		Name:        "fromAtoB",
@@ -13,19 +13,26 @@ func ASendToBThenBToC() {
 	message := mockup.MessageType{Type: "int"}
 	sendAToB := mockup.Send(channelAToB, message)
 
-	channelBToC := mockup.Channel{
-		Name:        "fromBtoC",
+	channelBToA := mockup.Channel{
+		Name:        "fromBtoA",
 		Source:      "B",
-		Destination: "C"}
-
-	sendBToC := mockup.Send(channelBToC, message)
+		Destination: "A"}
 
 	mockup.CreateStubProgram("ABCExample",
-		sendAToB,
-		sendBToC,
+		mockup.Loop("testLoop",
+			mockup.Send(channelAToB, mockup.MessageType{Type: "Int"}),
+			mockup.Switch(channelBToA,
+				mockup.Case("intIsGood",
+					mockup.Break(),
+				),
+				mockup.Case("intIsBad",
+					mockup.Continue("testLoop"),
+				),
+			),
+		),
 	)
 }
 
 func main() {
-	ASendToBThenBToC()
+	MakeLoopStub()
 }
